e621fbfb5078cf108ffe4e4b281fc8608a6d6b5f
Paul Chen <chenp02@gmail.com>
2019-02-02 18:07:54 -0500

    initial check in to github

---
+++ turbogears-pythonapp/build_image_instruction/readme.md
@@ -0,0 +1,14 @@
+## Build image
+___
+1) Copy example docker build files from the `build_image_instruction` folder to the project root folder
+
+```
+   cp  ./build_image_instruction/*  .
+```
+   
+2) Run docker build
+
+```
+   docker build -t [IMAGE_NAME]  .
+```
+
---
+++ turbogears-pythonapp/myapp.py
@@ -0,0 +1,19 @@
+from wsgiref.simple_server import make_server
+from tg import expose, TGController, AppConfig
+
+
+class MyController(TGController):
+
+    @expose()
+    def index(self):
+        return 'Hello TurboGears!'
+
+
+config = AppConfig(minimal=True, root_controller=MyController())
+application = config.make_wsgi_app()
+
+print
+# "Serving on port 8080..."
+# server = make_server('', 8080, application)
+# server.serve_forever()
+
